ad6122f23f9e90340b03ce52a68ecd9f45828154
abdul-hannan232 <abdulhani232@gmail.com>
2024-04-16 21:37:55 +0500

    home page responsiveness

--- app/(main)/home/page.js
+++ app/(main)/home/page.js
@@ -2,41 +2,42 @@ import React from 'react'
 import Navbar from '../../component/navbar'
 import Image from 'next/image'
 import { CiSearch } from "react-icons/ci"
-import  Footer from "../../component/footer"
+import Footer from "../../component/footer"
 import BasketBall from "../../component/basketBall"
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className='mt-12 banner  h-[100vh]'  >
+      <div className='mt-12 md:banner mobilebanner   h-[100vh]'  >
         <div className='relative  flex flex-col justify-center items-center h-[90vh] '>
-          <div className='bg-white absolute w-[55%] mx-auto h-80 flex flex-col justify-center it-center  bg-opacity-60 rounded-xl  right-0 left-0'>
-            <div className='relative mx-10 border bg-white text-black flex items-center p-7 rounded-full '>
-              <input type='text' placeholder='Search Here' className='text-black' />
-              <CiSearch className="absolute text-black w-20 right-0 text-4xl border-2 border-[#808080] border-t-transparent border-r-transparent border-b-transparent  top-6" />
+          <div className='bg-white absolute lg:w-[55%] w-[90%] mx-auto md:h-80 h-48 flex flex-col justify-center it-center  bg-opacity-60 rounded-xl  right-0 left-0'>
+            <div className='relative lg:mx-10 mx-5 border bg-white text-black flex items-center md:p-7 p-3 rounded-full '>
+              <input type='text' placeholder='Search Here' className='text-black md:text-lg text-xs ' />
+              <CiSearch className="absolute text-black md:w-20 w-8 px-1 my-2 md:h-10 h-6 right-0 text-4xl border-2 border-[#AFAFAF] border-t-transparent border-r-transparent border-b-transparent  md:top- " />
             </div>
             <div className='flex justify-center gap-3 mt-7 '>
-              <div className='bg-[#011344] text-center text-[#FFA500] flex flex-col justify-center items-cente rounded-[20px] p-4 w-24 '>
+              <div className='bg-white text-center text-black bg-opacity-60 border-[#000000] border-2 flex justify-center items-center md:rounded-[10px] rounded-[7px] md:p-4 p-2 md:w-36 w-22  md:h-16 h-10'>
 
-                <Image src="/basket.png" alt="basket" width={100} height={50} className='w-12 mx-auto h-12 ' />
-                <p className='text-[10px]'>X Courts</p>
+                <Image src="/Basketball Net.png" alt="basket" width={100} height={50} className='md:w-10 mx-auto mt-1 md:h-12 w-6 h-8 ' />
+                <p className='md:text-sm text-[8px]  font-bold'>X Courts</p>
               </div>
-              <div className='bg-[#011344] text-center text-[#FFA500] rounded-[20px] p-4 w-24 '>
+              <div className='bg-white text-center text-black bg-opacity-60 border-[#000000] border-2 flex justify-center items-center md:rounded-[10px] rounded-[7px] md:p-4 p-2 md:w-36 w-22  md:h-16 h-10'>
 
-                <Image src="/user.png" alt="basket" width={100} height={50} className='w-10 mx-auto h-14 ' />
-                <p className='text-[10px]'>X users</p>
+                <Image src="/Basketball Player.png" alt="basket" width={100} height={50} className='md:w-10 mx-auto  md:h-10 w-6 h-6 ' />
+                <p className='md:text-sm text-[8px]  font-bold'>X Users</p>
               </div>
-              <div className='bg-[#011344] text-center flex flex-col justify-center items-center text-[#FFA500] rounded-[20px] p-4 w-24 '>
+              <div className='bg-white text-center text-black bg-opacity-60 border-[#000000] border-2 flex justify-center items-center md:rounded-[10px] rounded-[7px] md:p-4 p-2 md:w-36 w-22  md:h-16 h-10'>
 
-                <Image src="/thumb.png" alt="basket" width={100} height={50} className='w-12 mx-auto h-12 ' />
-                <p className='text-[10px]'>X reviews</p>
+                <Image src="/Rating.png" alt="basket" width={100} height={50} className='md:w-10 mx-auto  md:h-10 w-6 h-6 ' />
+                <p className='md:text-[13px] text-[8px]  font-bold'>X Reviews</p>
               </div>
+          
             </div>
           </div>
         </div>
       </div>
       <BasketBall />
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   )
 }
--- app/component/footer.js
+++ app/component/footer.js
@@ -4,48 +4,62 @@ import Link from 'next/link'
 import Image from "next/image"
 const Footer = () => {
     return (
-        <div className='shadow p-10 mt-10'>
-            <div className='flex justify-between mx-20'>
-                <div> 
-                    <h1 className='2xl:text-4xl text-3xl font-bold text-[#FFA500]'>Logo</h1>
-                    <p className='font-light 2xl:text-lg text-sm w-72 text-[#F8F8F8] mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
-                    <div className='relative w-72 border  text-black shadow flex items-center p-3 rounded-full 2xl:mt-10 mt-7 '>
+        <div className='shadow md:p-10 mt-10'>
+            <div className='md:flex p-5  justify-between md:mx-20'>
+                <div>
+                    <h1 className='2xl:text-4xl md:text-3xl text-xl font-bold text-[#FFA500]'>Logo</h1>
+                    <p className='font-light 2xl:text-lg md:text-sm text-xs md:w-72 text-[#F8F8F8] mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
+                    <div className='relative w-72 border  md:block hidden text-black shadow flex items-center p-3 rounded-full 2xl:mt-10 mt-7 '>
                         <input type='text' placeholder='Send email' className='text-black bg-[#333333]' />
                         <FaArrowRight className="absolute text-black w-11 h-11 rounded-full right-0 text-4xl border-2 2xl:p-2 p-3 font-light  bg-[#ABABAB]   top-[2px]" />
                     </div>
                 </div>
-                <div>
-                    <ul className='2xl:space-y-3 space-y-2'>
-                        <li className='text-[#FFA500] 2xl:text-xl text-lg font-bold '> Company</li>
-                        <li><Link href="#" className='!text-sm'>Court</Link></li>
-                        <li><Link href="#" className='!text-sm'>Shop</Link></li>
-                        <li><Link href="#" className='!text-sm'>FAQs</Link></li>
-                        <li><Link href="#" className='!text-sm'>Contact Us</Link></li>
-                        <li><Link href="#" className='!text-sm'>Terms & Conditions</Link></li>
-                        <li><Link href="#" className='!text-sm'>About Us</Link></li>
-                        <li><Link href="#" className='!text-sm'>Privacy Policy</Link></li>
-                    </ul>
-                </div>
-                <div>
-                    <h1 className='text-2xl text-[#FFA500]'>Become a Member</h1>
-                    <p className='font-light 2xl:text-lg text-sm w-72 text-[#F8F8F8] mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                <hr className='my-10 block md:hidden' />
+                <div className='flex justify-between md:gap-40 md:mt-0 mt-10 gap-8'>
+                    <div>
+                        <ul className='2xl:space-y-3 md:space-y-2 md:w-44 w-28 '>
+                            <li className='text-[#FFA500] 2xl:text-xl md:text-lg text-sm font-bold '> Company</li>
+                            <li><Link href="#" className='md:!text-sm text-xs'>Court</Link></li>
+                            <li><Link href="#" className='md:!text-sm text-xs'>Shop</Link></li>
+                            <li><Link href="#" className='md:!text-sm text-xs'>FAQs</Link></li>
+                            <li><Link href="#" className='md:!text-sm text-xs'>Contact Us</Link></li>
 
-                    </p>
-                    <div className='relative 2xl:w-52 w-40 border mt-10 text-black shadow text-center border-[#FFA500] 2xl:p-3 p-2 cursor-pointer rounded-full '>
-                     <h1 className='text-[#FFA500] 2xl:text-2xl text-lg text-center'> Sign Up</h1>
+                            <li><Link href="#" className='md:!text-sm text-xs'>About Us</Link></li>
+                            <li><Link href="#" className='md:!text-sm text-xs'>Privacy Policy</Link></li>
+                            <li><Link href="#" className='md:!text-sm text-xs'>Terms & Conditions</Link></li>
+                        </ul>
+
+                    </div>
+                    <div>
+                        <h1 className='md:text-2xl text-sm text-[#FFA500]'>Become a Member</h1>
+                        <p className='font-light 2xl:text-lg md:text-sm text-xs md:w-72 text-[#F8F8F8] mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+
+                        </p>
+                        <div className='relative 2xl:w-52 md:w-40 w-28 border md:mt-10 mt-5 text-black shadow text-center border-[#FFA500] 2xl:p-3 p-2 cursor-pointer rounded-full '>
+                            <h1 className='text-[#FFA500] 2xl:text-2xl md:text-lg text-xs  text-center'> Sign Up</h1>
+                        </div>
                     </div>
                 </div>
             </div>
-            <hr className='m-20'/>
-            <div className='flex justify-between items-center mx-20'>
-            <div className='flex 2xl:gap-5 gap-3 items-center w-80'>
-              <Image src="/insta.png" alt="insta" width={56} height={56} className='w-10 h-10'/>
-              <Image src="/twitter.png" alt="insta" width={56} height={56} className='w-10 h-10'/>
-              <Image src="/facebook.png" alt="insta" width={56} height={56} className='w-10 h-10'/>
+            <div className='relative w-44 m-5  border  block md:hidden text-black shadow flex items-center p-[10px] rounded-full 2xl:mt-10 mt-7 '>
+                <input type='text' placeholder='Send email' className='text-black  text-xs bg-[#333333]' />
+                <FaArrowRight className="absolute text-black w-8 h-8 rounded-full right-0 text-4xl border-2  p-2 font-light  bg-[#ABABAB]   top-[2px]" />
             </div>
-            <div >
-                <p className='text-[#FFA500] text-xs'>Made by Mayonity</p>
+            <hr className='md:m-10  my-5' />
+            <div className='flex p-5  justify-between items-center md:mx-20'>
+                <div className='flex 2xl:gap-5 gap-3 items-center md:mx-0 mx-auto md:w-80'>
+                    <Image src="/insta.png" alt="insta" width={56} height={56} className='md:w-10 w-8 md:h-10 h-8' />
+                    <Image src="/twitter.png" alt="insta" width={56} height={56} className='md:w-10 w-8 md:h-10 h-8' />
+                    <Image src="/facebook.png" alt="insta" width={56} height={56} className='md:w-10 w-8 md:h-10 h-8' />
+                </div>
+                <div >
+                    <p className='text-[#FFA500] md:block hidden text-xs'>Made by Mayonity</p>
+                </div>
+
             </div>
+            <div>
+                <p className='bg-[#FFA500] md:hidden block text-sm text-center p-3 font-bold w-full text-black'>Made by Mayonity</p>
+
             </div>
         </div>
     )
