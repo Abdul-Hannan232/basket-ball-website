e69773c6a1415482a09fe3853e69a785b87a2b59
abdul-Hannan232 <abdulhani232@gmail.com>
2024-08-10 21:56:20 +0500

    testing

--- .history/app/component/courts_20240810213815.js
+++ .history/app/component/courts_20240810213815.js
@@ -7,7 +7,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import testimonaldata from '../data/testimonaldata.json';
 import Image from 'next/image';
-import { motion } from "framer-motion"
+// import { motion } from "framer-motion"
 
 const Testimonial = () => {
 
