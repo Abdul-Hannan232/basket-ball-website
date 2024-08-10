4c42820c82bda6a404790b8f862c80b2f3812347
abdul-Hannan232 <abdulhani232@gmail.com>
2024-08-10 07:34:19 +0500

    section add in contactus page

---
+++ .history/app/(main)/home/page_20240807233352.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Navbar from '../../component/navbar'
+import Image from 'next/image'
+import { CiSearch } from "react-icons/ci"
+import Footer from "../../component/footer"
+import BasketBall from "../../component/basketBall"
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='mt-12 md:banner mobilebanner   md:h-[100vh] h-[250px]'  >
+        <div className='relative  flex flex-col md:justify-center md:mt-0 pt-14 items-center md:h-[90vh] '>
+          <div className='bg-white absolute lg:w-[55%] w-[90%] mx-auto md:h-80 h-36 flex flex-col justify-center it-center  bg-opacity-60 rounded-xl  right-0 left-0'>
+            <div className='relative lg:mx-10 mx-5 border bg-white text-black flex items-center   rounded-full'>
+              <input type='text' placeholder='Search Here' className='text-black md:text-lg text-xs border-transparent outline-none hover:border-[#FFA500] border-2 md:p-6 p-3 w-full rounded-full ' />
+              <CiSearch className="absolute text-black md:w-20 w-8 px-1 my-2 md:h-10 h-6 right-0 text-4xl border-2 border-[#AFAFAF] border-t-transparent border-r-transparent border-b-transparent  md:top- " />
+            </div>
+            <div className='flex justify-center gap-3 mt-7 '>
+              <div className='bg-white text-center text-black bg-opacity-60 border-[#000000] border-2 flex justify-center items-center md:rounded-[10px] rounded-[7px] md:p-4 p-2 md:w-36 w-22  md:h-16 h-10'>
+
+                <Image src="/Basketball Net.png" alt="basket" width={100} height={50} className='md:w-10 mx-auto mt-1 md:h-12 w-6 h-8 ' />
+                <p className='md:text-sm text-[8px]  font-bold'>X Courts</p>
+              </div>
+          
+              <div className='bg-white text-center text-black bg-opacity-60 border-[#000000] border-2 flex justify-center items-center md:rounded-[10px] rounded-[7px] md:p-4 p-2 md:w-36 w-22  md:h-16 h-10'>
+
+                <Image src="/Rating.png" alt="basket" width={100} height={50} className='md:w-10 mx-auto  md:h-10 w-6 h-6 ' />
+                <p className='md:text-[13px] text-[8px]  font-bold'>X Reviews</p>
+              </div>
+          
+            </div>
+          </div>
+        </div>
+      </div>
+      <BasketBall />
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
---
+++ .history/app/component/navbar_20240807233756.js
@@ -0,0 +1,64 @@
+"use client"
+import React from 'react'
+import { CgProfile } from "react-icons/cg";
+import { RxCross2 } from "react-icons/rx";
+import Link from "next/link"
+import { IoCartOutline } from "react-icons/io5";
+import { IoIosMenu } from "react-icons/io";
+import { useState } from 'react';
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+    const toOpen = () => {
+        setOpen(true)
+    }
+    const toClose = () => {
+        setOpen(false)
+    }
+    return (
+        <div className='bg-[#FFA500] fixed top-0 left-0 right-0 p-4 flex z-40 items-center justify-between'>
+            <div className='flex items-center gap-1'>
+                <IoIosMenu className='text-black text-xl cursor-pointer md:hidden block' onClick={toOpen}/>
+              <Image src="">
+            </div>
+            <div className='md:block hidden  '>
+                <ul className='flex  items-center   space-x-10'>
+                    <li><a className='font-medium text-lg  cursor-pointer hover:border-2 hover:border-b-black border-x-transparent border-t-transparent   text-black' href='/home'>Home </a></li>
+                    <li><a className='font-medium text-lg cursor-pointer hover:border-2 hover:border-b-black border-x-transparent border-t-transparent text-black' href='#'>Court</a></li>
+                    <li><a className='font-medium text-lg  cursor-pointer hover:border-2 hover:border-b-black border-x-transparent border-t-transparent text-black' href='#'>Shop</a></li>
+                    <li><a className='font-medium text-lg  cursor-pointer hover:border-2 hover:border-b-black border-x-transparent border-t-transparent text-black' href='#'>Donate</a></li>
+                </ul>
+            </div>
+            <div className='flex gap-2   items-center'>
+
+                <IoCartOutline className=' text-3xl text-black font-medium ' />
+                {/* <CgProfile className=' text-2xl text-black font-medium ' /> */}
+                <button className='md:block hidden  py-2 px-5 border-[#011344] bg-[#011344] border ml-4 text-sm  rounded-md'>Sign Up</button>
+                <button className='md:block hidden  py-2 px-6 border border-black text-black text-sm  ml-1 rounded-md'>Login</button>
+            </div>
+            {open && (
+                <>
+                    <div className='absolute top-0 bg-gray-600 w-full h-screen left-0 right-0 '>
+                        <div className='flex justify-between bg-[#FFA500] items-center p-5'>
+                            <h1 className=''>Logo</h1>
+                            <div className='flex items-center gap-3'>
+                                <IoCartOutline className=' text-2xl text-black font-light ' />
+                                <RxCross2 className=' text-2xl text-black font-light cursor-pointer ' onClick={toClose}/>
+                            </div>
+                        </div>
+                        <ul className='space-y-3 text-sm mt-10 p-3 font-bold'>
+                            <li><Link href="/" className='focus:text-[#FFA500]'>Home</Link></li>
+                            <li><Link href="/" className='focus:text-[#FFA500]'>Court</Link></li>
+                            <li><Link href="/" className='focus:text-[#FFA500]'>Shop</Link></li>
+                            <li><Link href="/" className='focus:text-[#FFA500]'>Donate</Link></li>
+                            <li><Link href="/" className='focus:text-[#FFA500]'>Login/Signup</Link></li>
+
+
+                        </ul>
+                    </div>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default Navbar
