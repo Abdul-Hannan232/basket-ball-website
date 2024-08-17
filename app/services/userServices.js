679ddc5b48fe2932656f5a78ca64716c869210f0
Abdul-Hannan232 <abdulhani232@gmail.com>
2024-08-17 18:51:06 +0500

    User Block/Unblock working now

--- app/services/userServices.js
+++ app/services/userServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
-export const allUsers = async () =>{
+export const allUsers = async () => {
     try {
         const token = Cookies.get('authToken')
         const response = await axios.get(
@@ -11,12 +11,34 @@ export const allUsers = async () =>{
                 },
             }
         );
-         
+
         return response;
     }
     catch (error) {
-        console.log("all user",error.response)
         return error.response || { message: "An error occurred", error };
     }
-    
+
+}
+
+export const updateUser = async (body) => {
+    try {
+       
+        const token = Cookies.get('authToken');
+
+        const response = await axios.put(
+            `${process.env.NEXT_PUBLIC_API_URL}/user/update`,
+            body, // Body is sent as the second argument
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        return response;
+    }
+    catch (error) {
+        return error.response || { message: "An error occurred", error };
+    }
+
 }
