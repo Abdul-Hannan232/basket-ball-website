20defc18475782d87f878842b7dd87013f209176
Abdul-Hannan232 <abdulhani232@gmail.com>
2024-08-17 08:45:34 +0500

    Build Error Removed

--- app/admin/page.js
+++ app/admin/page.js
@@ -22,9 +22,9 @@ const Page = () => {
   }, []);
 
   return (
-    <div className='flex flex-col justify-center items-center h-screen '>
+    <div className='flex flex-col justify-center items-center h-screen'>
       <h1 className='text-2xl font-bold text-center'>Welcome to Your Dashboard, {username}</h1>
-      <p className='text-lg text-gray-600 mt-2 text-center'>"This dashboard is currently under development and will be ready for use soon."</p>
+      <p className='text-lg text-gray-600 mt-2 text-center'>This dashboard is currently under development and will be ready for use soon.</p>
       <br />
       <button className='w-40 text-center p-3 rounded-md bg-black shadow-xl text-white' onClick={handleLogout}>
         Logout
